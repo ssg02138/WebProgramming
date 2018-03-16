7bd98b880440ee691d581b948206934cf46a32e3
ssg02138 <ssg02138@github.com>
2018-03-16 23:16:31 +0900

    i

--- week3/homework/phonejs.js
+++ week3/homework/phonejs.js
@@ -1,4 +1,10 @@
 /* phonejs.js*/
+
+function removeRow(r){ 
+    var i=r.parentNode.parentNode.rowIndex;
+    document.getElementById('insertTable').deleteRow(i);
+}
+
 document.getElementById("btnSave").onclick=function(){
     lastName = document.getElementById("ln").value;
     firstName = document.getElementById("fn").value;
@@ -10,12 +16,12 @@ document.getElementById("btnSave").onclick=function(){
     td2 = document.createElement("td");
     td3 = document.createElement("td");
     td4 = document.createElement("td");
-    td4.id="del";
-
     td1.innerText = lastName;
     td2.innerText = firstName;
     td3.innerText = phoneNumber;
-    td4.innerText = "Del"
+
+    td4.innerHTML = "<input type='button' name=cmdDelete value='Del' onClick='removeRow(this);'>";
+
     tr.appendChild(td1);
     tr.appendChild(td2);
     tr.appendChild(td3);
